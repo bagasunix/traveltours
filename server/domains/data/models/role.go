abc5cbbf558f170970c35adc52ccd742b8ede05a
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-14 23:23:33 +0700

    create model role

---
+++ server/domains/data/models/role.go
@@ -0,0 +1,55 @@
+package models
+
+type Role struct {
+	BaseModel
+	Name     string
+	TypeRole string
+	Desc     string
+	IsActive int8
+}
+
+// Builder Object for Role
+type RoleBuilder struct {
+	BaseModelBuilder
+	name     string
+	typeRole string
+	desc     string
+	isActive int8
+}
+
+// Constructor for RoleBuilder
+func NewRoleBuilder() *RoleBuilder {
+	o := new(RoleBuilder)
+	return o
+}
+
+// Build Method which creates Role
+func (b *RoleBuilder) Build() *Role {
+	o := new(Role)
+	o.BaseModel = *b.BaseModelBuilder.Build()
+	o.Name = b.name
+	o.TypeRole = b.typeRole
+	o.Desc = b.desc
+	o.IsActive = b.isActive
+	return o
+}
+
+// Setter method for the field name of type string in the object RoleBuilder
+func (r *RoleBuilder) SetName(name string) {
+	r.name = name
+}
+
+// Setter method for the field typeRole of type string in the object RoleBuilder
+func (r *RoleBuilder) SetTypeRole(typeRole string) {
+	r.typeRole = typeRole
+}
+
+// Setter method for the field desc of type string in the object RoleBuilder
+func (r *RoleBuilder) SetDesc(desc string) {
+	r.desc = desc
+}
+
+// Setter method for the field isActive of type int8 in the object RoleBuilder
+func (r *RoleBuilder) SetIsActive(isActive int8) {
+	r.isActive = isActive
+}
