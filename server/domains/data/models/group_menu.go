6e3ecb52c3c8a4fa82c03c35924fe5ed9c779aab
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-14 22:54:15 +0700

    create set model group menu

--- server/domains/data/models/group_menu.go
+++ server/domains/data/models/group_menu.go
@@ -30,3 +30,18 @@ func (b *GroupMenuBuilder) Build() *GroupMenu {
 	o.IsActive = b.isActive
 	return o
 }
+
+// Setter method for the field name of type string in the object GroupMenuBuilder
+func (g *GroupMenuBuilder) SetName(name string) {
+	g.name = name
+}
+
+// Setter method for the field desc of type string in the object GroupMenuBuilder
+func (g *GroupMenuBuilder) SetDesc(desc string) {
+	g.desc = desc
+}
+
+// Setter method for the field isActive of type int8 in the object GroupMenuBuilder
+func (g *GroupMenuBuilder) SetIsActive(isActive int8) {
+	g.isActive = isActive
+}
