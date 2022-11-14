af8382052790c147fc3b991d67c7eb00efe590e9
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-15 00:31:01 +0700

    create permission

---
+++ server/domains/data/models/permission.go
@@ -0,0 +1,51 @@
+package models
+
+type Permission struct {
+	BaseModel
+	Name   string
+	Slug   string
+	Method []string
+	Url    []string
+}
+
+// Builder Object for Permission
+type PermissionBuilder struct {
+	BaseModelBuilder
+	name   string
+	slug   string
+	method []string
+	url    []string
+}
+
+// Constructor for PermissionBuilder
+func NewPermissionBuilder() *PermissionBuilder {
+	o := new(PermissionBuilder)
+	return o
+}
+
+// Build Method which creates Permission
+func (b *PermissionBuilder) Build() *Permission {
+	o := new(Permission)
+	o.BaseModel = *b.BaseModelBuilder.Build()
+	o.Name = b.name
+	o.Slug = b.slug
+	o.Method = b.method
+	o.Url = b.url
+	return o
+}
+
+
+// Getter method for the field slug of type string in the object PermissionBuilder
+func (p *PermissionBuilder) Slug() string {		
+	return p.slug
+}
+
+// Setter method for the field method of type []string in the object PermissionBuilder
+func (p *PermissionBuilder) SetMethod(method []string) {		
+	p.method = method
+}
+
+// Setter method for the field url of type []string in the object PermissionBuilder
+func (p *PermissionBuilder) SetUrl(url []string) {		
+	p.url = url
+}
