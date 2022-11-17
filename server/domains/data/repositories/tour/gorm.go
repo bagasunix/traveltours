73fd2fe529762a6f0c2a4395bf563ec3d0975b1e
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-17 16:56:09 +0700

    create repo tour

---
+++ server/domains/data/repositories/tour/gorm.go
@@ -0,0 +1,60 @@
+package tour
+
+import (
+	"context"
+
+	"github.com/bagasunix/traveltours/pkg/errors"
+	"github.com/bagasunix/traveltours/server/domains/data/models"
+	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type gormProvider struct {
+	logger *zap.Logger
+	db     *gorm.DB
+}
+
+// GetConnection implements Repository
+func (g *gormProvider) GetConnection() (T any) {
+	return g.db
+}
+
+// GetModelName implements Repository
+func (g *gormProvider) GetModelName() string {
+	return "Tour"
+}
+
+// Create implements Repository
+func (g *gormProvider) Create(ctx context.Context, m *models.Tour) error {
+	return errors.ErrDuplicateValue(g.logger, g.GetModelName(), g.db.WithContext(ctx).Create(m).Error)
+}
+
+// Delete implements Repository
+func (g *gormProvider) Delete(ctx context.Context, id uuid.UUID) error {
+	return errors.ErrSomethingWrong(g.logger, g.db.WithContext(ctx).Delete(models.NewTourBuilder().Build(), "id=?", id.String()).Error)
+}
+
+// Update implements Repository
+func (g *gormProvider) Update(ctx context.Context, m *models.Tour) error {
+	return errors.ErrSomethingWrong(g.logger, g.db.WithContext(ctx).Updates(m).Error)
+}
+
+// GetAll implements Repository
+func (g *gormProvider) GetAll(ctx context.Context) (result models.SliceResult[models.Tour]) {
+	result.Error = errors.ErrRecordNotFound(g.logger, g.GetModelName(), g.db.WithContext(ctx).Find(&result.Value).Error)
+	return result
+}
+
+// GetById implements Repository
+func (g *gormProvider) GetById(ctx context.Context, id uuid.UUID) (result models.SingleResult[*models.Tour]) {
+	result.Error = errors.ErrRecordNotFound(g.logger, g.GetModelName(), g.db.WithContext(ctx).First(&result.Value, "id=?", id.String()).Error)
+	return result
+}
+
+func NewGorm(logger *zap.Logger, db *gorm.DB) Repository {
+	g := new(gormProvider)
+	g.logger = logger
+	g.db = db
+	return g
+}
---
+++ server/domains/data/repositories/tour/repository.go
@@ -0,0 +1,26 @@
+package tour
+
+import (
+	"context"
+
+	"github.com/bagasunix/traveltours/server/domains/data/models"
+	"github.com/bagasunix/traveltours/server/domains/data/repositories/base"
+	"github.com/gofrs/uuid"
+)
+
+type Commond interface {
+	Create(ctx context.Context, m *models.Tour) error
+	Update(ctx context.Context, m *models.Tour) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type Query interface {
+	GetAll(ctx context.Context) (result models.SliceResult[models.Tour])
+	GetById(ctx context.Context, id uuid.UUID) (result models.SingleResult[*models.Tour])
+}
+
+type Repository interface {
+	base.Repository
+	Commond
+	Query
+}
