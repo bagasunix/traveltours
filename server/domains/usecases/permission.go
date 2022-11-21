5105ab2ba27c284bb68e6e5f24b40c1613dab14e
Aldino Pratama <aldinopratama15@gmail.com>
2022-11-21 23:14:01 +0700

    update usecase permission

--- server/domains/usecases/permission.go
+++ server/domains/usecases/permission.go
@@ -7,8 +7,12 @@ import (
 	"github.com/bagasunix/traveltours/server/domains/data/models"
 	"github.com/bagasunix/traveltours/server/domains/data/repositories"
 	"github.com/bagasunix/traveltours/server/domains/entities"
+	"github.com/bagasunix/traveltours/server/domains/entities/mappers"
 	"github.com/bagasunix/traveltours/server/endpoints/requests"
 	"github.com/bagasunix/traveltours/server/endpoints/responses"
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/gofrs/uuid"
+	"github.com/gosimple/slug"
 	"go.uber.org/zap"
 )
 
@@ -30,10 +34,11 @@ func (p *permission) CreatePermission(ctx context.Context, request *requests.Cre
 	if err = request.Validate(); err != nil {
 		return nil, err
 	}
+	slugs := slug.Make(request.Name)
 	mBuild := models.NewPermissionBuilder()
 	mBuild.SetId(helpers.GenerateUUIDV1(p.logger))
 	mBuild.SetName(request.Name)
-	mBuild.SetSlug(request.Slug)
+	mBuild.SetSlug(slugs)
 	mBuild.SetUrl(request.Url)
 	mBuild.SetMethod(request.Method)
 	if err = p.repo.GetPermission().Create(ctx, mBuild.Build()); err != nil {
@@ -45,22 +50,62 @@ func (p *permission) CreatePermission(ctx context.Context, request *requests.Cre
 
 // DeletePermission implements Permission
 func (p *permission) DeletePermission(ctx context.Context, request *requests.EntityId) (response *responses.Empty, err error) {
-	panic("unimplemented")
+	if err = request.Validate(); err != nil {
+		return nil, err
+	}
+	if err = p.repo.GetPermission().Delete(ctx, request.Id.(uuid.UUID)); err != nil {
+		return nil, err
+	}
+	return new(responses.Empty), nil
 }
 
 // ListPermission implements Permission
 func (p *permission) ListPermission(ctx context.Context, request *requests.BaseList) (response *responses.ListEntity[entities.Permission], err error) {
-	panic("unimplemented")
+	if request.Limit == 0 {
+		request.Limit = 25
+	}
+	responseBuilder := responses.NewListEntityBuilder[entities.Permission]()
+	if validation.IsEmpty(request.Keyword) {
+		result := p.repo.GetPermission().GetAll(ctx, request.Limit)
+		responseBuilder.SetData(mappers.ListPermissionModelToListEntity(result.Value))
+		return responseBuilder.Build(), result.Error
+	}
+	result := p.repo.GetPermission().GetByKeywords(ctx, request.Keyword, request.Limit)
+	responseBuilder.SetData(mappers.ListPermissionModelToListEntity(result.Value))
+	return responseBuilder.Build(), result.Error
 }
 
 // UpdatePermission implements Permission
 func (p *permission) UpdatePermission(ctx context.Context, request *requests.UpdatePermission) (response *responses.Empty, err error) {
-	panic("unimplemented")
+	if err = request.Validate(); err != nil {
+		return nil, err
+	}
+	slugs := slug.Make(request.Name)
+	mBuild := models.NewPermissionBuilder()
+	mBuild.SetId(request.Id.(uuid.UUID))
+	mBuild.SetName(request.Name)
+	mBuild.SetSlug(slugs)
+	mBuild.SetUrl(request.Url)
+	mBuild.SetMethod(request.Method)
+	model := mBuild.Build()
+	if err = p.repo.GetPermission().Update(ctx, model); err != nil {
+		return nil, err
+	}
+
+	return new(responses.Empty), nil
 }
 
 // ViewPermission implements Permission
 func (p *permission) ViewPermission(ctx context.Context, request *requests.EntityId) (response *responses.ViewEntity[*entities.Permission], err error) {
-	panic("unimplemented")
+	if err = request.Validate(); err != nil {
+		return nil, err
+	}
+
+	result := p.repo.GetPermission().GetById(ctx, request.Id.(uuid.UUID))
+
+	responseBuilder := responses.NewViewEntityBuilder[*entities.Permission]()
+	responseBuilder.SetData(mappers.PermissionModelToEntity(result.Value))
+	return responseBuilder.Build(), result.Error
 }
 
 func NewPermission(logger *zap.Logger, repo repositories.Repositories) Permission {
