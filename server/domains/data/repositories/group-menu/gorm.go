b5c0d0380e143b64f148a3dccc9e097e25a033cd
Aldino Pratama <aldinopratama15@gmail.com>
2022-11-22 18:31:00 +0700

    create repo group menu

---
+++ server/domains/data/repositories/group-menu/gorm.go
@@ -0,0 +1,60 @@
+package groupmenu
+
+import (
+	"context"
+
+	"github.com/bagasunix/traveltours/pkg/errors"
+	"github.com/bagasunix/traveltours/server/domains/data/models"
+	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type gormProvider struct {
+	logger *zap.Logger
+	db     *gorm.DB
+}
+
+// GetConnection implements Repository
+func (g *gormProvider) GetConnection() (T any) {
+	return g.db
+}
+
+// GetModelName implements Repository
+func (g *gormProvider) GetModelName() string {
+	return "Group Menu"
+}
+
+// Create implements Repository
+func (g *gormProvider) Create(ctx context.Context, m *models.GroupMenu) error {
+	return errors.ErrDuplicateValue(g.logger, g.GetModelName(), g.db.WithContext(ctx).Create(m).Error)
+}
+
+// Delete implements Repository
+func (g *gormProvider) Delete(ctx context.Context, id uuid.UUID) error {
+	return errors.ErrSomethingWrong(g.logger, g.db.WithContext(ctx).Delete(models.NewGroupMenuBuilder().Build(), "id = ?", id.String()).Error)
+}
+
+// Update implements Repository
+func (g *gormProvider) Update(ctx context.Context, m *models.GroupMenu) error {
+	return errors.ErrDuplicateValue(g.logger, g.GetModelName(), g.db.WithContext(ctx).Updates(m).Error)
+}
+
+// GetAll implements Repository
+func (g *gormProvider) GetAll(ctx context.Context, limit int64) (result models.SliceResult[models.GroupMenu]) {
+	result.Error = errors.ErrRecordNotFound(g.logger, g.GetModelName(), g.db.WithContext(ctx).Limit(int(limit)).Find(&result.Value).Error)
+	return result
+}
+
+// GetById implements Repository
+func (g *gormProvider) GetById(ctx context.Context, id uuid.UUID) (result models.SingleResult[*models.GroupMenu]) {
+	result.Error = errors.ErrRecordNotFound(g.logger, g.GetModelName(), g.db.WithContext(ctx).First(&result.Value, "id = ?", id.String()).Error)
+	return result
+}
+
+func NewGorm(logger *zap.Logger, db *gorm.DB) Repository {
+	g := new(gormProvider)
+	g.logger = logger
+	g.db = db
+	return g
+}
---
+++ server/domains/data/repositories/group-menu/repository.go
@@ -0,0 +1,26 @@
+package groupmenu
+
+import (
+	"context"
+
+	"github.com/bagasunix/traveltours/server/domains/data/models"
+	"github.com/bagasunix/traveltours/server/domains/data/repositories/base"
+	"github.com/gofrs/uuid"
+)
+
+type Commond interface {
+	Create(ctx context.Context, m *models.GroupMenu) error
+	Update(ctx context.Context, m *models.GroupMenu) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type Query interface {
+	GetAll(ctx context.Context, limit int64) (result models.SliceResult[models.GroupMenu])
+	GetById(ctx context.Context, id uuid.UUID) (result models.SingleResult[*models.GroupMenu])
+}
+
+type Repository interface {
+	base.Repository
+	Commond
+	Query
+}
