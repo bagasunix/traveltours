fcbd20c18af38bb66ccc95db0378993b11f77b7b
Aldino Pratama <aldinopratama15@gmail.com>
2022-11-24 23:46:07 +0700

    update model created

--- server/domains/data/models/created.go
+++ server/domains/data/models/created.go
@@ -7,36 +7,38 @@ import (
 )
 
 type Created struct {
-	CreatedBy uuid.UUID `gorm:"type:uuid.UUID;index;<-:create"`
-	CreatedAt time.Time `gorm:"autoCreateTime;Index;sort:desc;<-:create"`
+	CreatedBy uuid.UUID `gorm:"type:uuid;index;<-:create"`
+	CreatedAt time.Time `gorm:"autoCreateTime;index;sort:desc;<-:create"`
 }
 
-// Builder Object for CreatedAt
+// Builder Object for Created
 type CreatedBuilder struct {
 	createdBy uuid.UUID
 	createdAt time.Time
 }
 
-// Constructor for CreatedAtBuilder
-func NewCreatedAtBuilder() *CreatedBuilder {
+// Constructor for CreatedBuilder
+func NewCreatedBuilder() *CreatedBuilder {
 	o := new(CreatedBuilder)
 	return o
 }
 
-// Build Method which creates CreatedAt
+// Build Method which creates Created
 func (b *CreatedBuilder) Build() *Created {
 	o := new(Created)
-	o.CreatedBy = b.createdBy
 	o.CreatedAt = b.createdAt
+	o.CreatedBy = b.createdBy
 	return o
 }
 
-// Setter method for the field createdBy of type uuid.UUID in the object CreatedAtBuilder
-func (c *CreatedBuilder) SetCreatedBy(createdBy uuid.UUID) {
+// Setter method for the field createdBy of type uuid.UUID in the object CreatedBuilder
+func (c *CreatedBuilder) SetCreatedBy(createdBy uuid.UUID) *CreatedBuilder {
 	c.createdBy = createdBy
+	return c
 }
 
-// Setter method for the field createdAt of type time.Time in the object CreatedAtBuilder
-func (c *CreatedBuilder) SetCreatedAt(createdAt time.Time) {
+// Setter method for the field createdAt of type time.Time in the object CreatedBuilder
+func (c *CreatedBuilder) SetCreatedAt(createdAt time.Time) *CreatedBuilder {
 	c.createdAt = createdAt
+	return c
 }
