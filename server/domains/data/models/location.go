b7fa88cd17096abae646ea0b4944d448fd161146
Aldino Pratama <aldinopratama15@gmail.com>
2022-11-25 19:13:26 +0700

    update

--- server/domains/data/models/location.go
+++ server/domains/data/models/location.go
@@ -2,7 +2,7 @@ package models
 
 import "github.com/gofrs/uuid"
 
-type LocationTour struct {
+type TourLocation struct {
 	BaseModel
 	TourId    uuid.UUID
 	Tour      *Tour `gorm:"foreignKey:TourId;constraint:OnUpdate:CASCADE,OnDelete:Restrict"`
@@ -14,8 +14,8 @@ type LocationTour struct {
 	Day           int
 }
 
-// Builder Object for LocationTour
-type LocationTourBuilder struct {
+// Builder Object for TourLocation
+type TourLocationBuilder struct {
 	BaseModelBuilder
 	tourId    uuid.UUID
 	tour      *Tour
@@ -27,15 +27,15 @@ type LocationTourBuilder struct {
 	day           int
 }
 
-// Constructor for LocationTourBuilder
-func NewLocationTourBuilder() *LocationTourBuilder {
-	o := new(LocationTourBuilder)
+// Constructor for TourLocationBuilder
+func NewTourLocationBuilder() *TourLocationBuilder {
+	o := new(TourLocationBuilder)
 	return o
 }
 
-// Build Method which creates LocationTour
-func (b *LocationTourBuilder) Build() *LocationTour {
-	o := new(LocationTour)
+// Build Method which creates TourLocation
+func (b *TourLocationBuilder) Build() *TourLocation {
+	o := new(TourLocation)
 	o.BaseModel = *b.BaseModelBuilder.Build()
 	o.TourId = b.tourId
 	o.Tour = b.tour
@@ -48,37 +48,37 @@ func (b *LocationTourBuilder) Build() *LocationTour {
 	return o
 }
 
-// Setter method for the field villageId of type *int64 in the object LocationTourBuilder
-func (l *LocationTourBuilder) SetVillageId(villageId *int64) {
+// Setter method for the field villageId of type *int64 in the object TourLocationBuilder
+func (l *TourLocationBuilder) SetVillageId(villageId *int64) {
 	l.villageId = villageId
 }
 
-// Setter method for the field village of type *Village in the object LocationTourBuilder
-func (l *LocationTourBuilder) SetVillage(village *Village) {
+// Setter method for the field village of type *Village in the object TourLocationBuilder
+func (l *TourLocationBuilder) SetVillage(village *Village) {
 	l.village = village
 }
 
-// Setter method for the field streetAddress of type string in the object LocationTourBuilder
-func (l *LocationTourBuilder) SetStreetAddress(streetAddress string) {
+// Setter method for the field streetAddress of type string in the object TourLocationBuilder
+func (l *TourLocationBuilder) SetStreetAddress(streetAddress string) {
 	l.streetAddress = streetAddress
 }
 
-// Setter method for the field description of type string in the object LocationTourBuilder
-func (l *LocationTourBuilder) SetDescription(description string) {
+// Setter method for the field description of type string in the object TourLocationBuilder
+func (l *TourLocationBuilder) SetDescription(description string) {
 	l.description = description
 }
 
-// Setter method for the field day of type int in the object LocationTourBuilder
-func (l *LocationTourBuilder) SetDay(day int) {
+// Setter method for the field day of type int in the object TourLocationBuilder
+func (l *TourLocationBuilder) SetDay(day int) {
 	l.day = day
 }
 
-// Setter method for the field tourId of type uuid.UUID in the object LocationTourBuilder
-func (l *LocationTourBuilder) SetTourId(tourId uuid.UUID) {
+// Setter method for the field tourId of type uuid.UUID in the object TourLocationBuilder
+func (l *TourLocationBuilder) SetTourId(tourId uuid.UUID) {
 	l.tourId = tourId
 }
 
-// Setter method for the field tour of type *Tour in the object LocationTourBuilder
-func (l *LocationTourBuilder) SetTour(tour *Tour) {
+// Setter method for the field tour of type *Tour in the object TourLocationBuilder
+func (l *TourLocationBuilder) SetTour(tour *Tour) {
 	l.tour = tour
 }
