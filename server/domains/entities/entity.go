114cc24c74cc25b7c597de95b19b2881bda9eb8a
Aldino Pratama <aldinopratama15@gmail.com>
2022-11-30 16:15:33 +0700

    update entity

--- server/domains/entities/entity.go
+++ server/domains/entities/entity.go
@@ -76,3 +76,33 @@ func (c *CreatedBuilder) SetCreatedAt(createdAt time.Time) {
 func (c *CreatedBuilder) SetCreatedBy(createdBy any) {
 	c.createdBy = createdBy
 }
+
+// Builder Object for Updated
+type UpdatedBuilder struct {
+	updatedAt time.Time
+	updatedBy uuid.UUID
+}
+
+// Constructor for UpdatedBuilder
+func NewUpdatedBuilder() *UpdatedBuilder {
+	o := new(UpdatedBuilder)
+	return o
+}
+
+// Build Method which creates Updated
+func (b *UpdatedBuilder) Build() *Updated {
+	o := new(Updated)
+	o.UpdatedAt = b.updatedAt
+	o.UpdatedBy = b.updatedBy
+	return o
+}
+
+// Setter method for the field updatedAt of type time.Time in the object UpdatedBuilder
+func (u *UpdatedBuilder) SetUpdatedAt(updatedAt time.Time) {
+	u.updatedAt = updatedAt
+}
+
+// Setter method for the field updatedBy of type uuid.UUID in the object UpdatedBuilder
+func (u *UpdatedBuilder) SetUpdatedBy(updatedBy uuid.UUID) {
+	u.updatedBy = updatedBy
+}
